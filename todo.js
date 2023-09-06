2ad59590729240618990efba949a79f863215912
hamesomer11 <hamzaomer45@gmail.com>
2023-09-06 22:29:15 +0100

    new update

--- todo.js
+++ todo.js
@@ -1,3 +1,4 @@
+// open and close popup form
 function openForm() {
     document.getElementById("myForm").style.display = "block";
 }
@@ -6,9 +7,11 @@ function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
 
+
 const notes = document.getElementById("note");
 const addNotes = document.getElementById("add-notes");
 
+// store user input to local storage
 let notesStorage = localStorage.getItem("note")
   ? JSON.parse(localStorage.getItem("note"))
   : [];
@@ -21,17 +24,21 @@ myForm.addEventListener("submit", (e) => {
     addNotes.value = "";
 });
 
+// Displays user input from localstorage when posted
 const listBuilder = (text) => {
     const note = document.createElement("li");
     note.innerHTML = text + ' <button id="trash" onclick="deleteNote(this)"> <i class="fa-solid fa-trash"></i> </button>';
     notes.appendChild(note);
 };
 
+// Shows all contents in the array
 const getNotes = JSON.parse(localStorage.getItem("note"));
 getNotes.forEach((note) => {
     listBuilder(note);
 });
 
+
+// Deletes notes on page
 const deleteNote = (btn) => {
     let el = btn.parentNode;
     const index = [...el.parentElement.children].indexOf(el);
