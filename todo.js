aca519bf1f68667284285a29388b430ee3b6c98f
hamesomer11 <hamzaomer45@gmail.com>
2023-09-06 20:04:22 +0100

    new update

--- todo.js
+++ todo.js
@@ -4,5 +4,38 @@ function openForm() {
   
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
-    console.log("Clicked")
 }
+
+const notes = document.getElementById("note");
+const addNotes = document.getElementById("add-notes");
+
+let notesStorage = localStorage.getItem("note")
+  ? JSON.parse(localStorage.getItem("note"))
+  : [];
+
+myForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    notesStorage.push(addNotes.value);
+    localStorage.setItem("note", JSON.stringify(notesStorage));
+    listBuilder(addNotes.value);
+    addNotes.value = "";
+});
+
+const listBuilder = (text) => {
+    const note = document.createElement("li");
+    note.innerHTML = text + ' <button id="trash" onclick="deleteNote(this)"> <i class="fa-solid fa-trash"></i> </button>';
+    notes.appendChild(note);
+};
+
+const getNotes = JSON.parse(localStorage.getItem("note"));
+getNotes.forEach((note) => {
+    listBuilder(note);
+});
+
+const deleteNote = (btn) => {
+    let el = btn.parentNode;
+    const index = [...el.parentElement.children].indexOf(el);
+    notesStorage.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesStorage));
+    el.remove();
+};
